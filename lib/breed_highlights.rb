f2ba6cb8295b0063b44ae3c408ae02310071fe7b
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-12-06 16:13:31 +0000

    Initial commit

---
+++ lib/breed_highlights.rb
@@ -0,0 +1,9 @@
+require_relative "./breed_highlights/version"
+require_relative "breed_highlights/cli"
+
+require 'pry'
+
+module BreedHighlights
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/breed_highlights/cli.rb
@@ -0,0 +1,21 @@
+class BreedHighlights::CLI 
+
+  def call 
+    puts "Welcome! Let's find out more about the most popular breeds in UK."
+    #Please select the one you want to see breed highlaghts."
+    binding.pry
+  end
+  
+  def get_dog_breeds
+    #to be scraped
+    @breeds = ["French Buldog", "English Buldog", "Chichuaua"]
+  end
+  
+  def get_selected_breed
+    #list breeds
+    @breeds.each_with_index do |index, breed|
+      puts "#{index} #{breed}"
+    end
+  end
+    
+end

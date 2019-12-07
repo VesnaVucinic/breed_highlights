b8836446c03e4f3354c7d3eeac8607b7c173abc0
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-12-07 18:38:46 +0000

    cli get select

--- lib/breed_highlights/cli.rb
+++ lib/breed_highlights/cli.rb
@@ -1,8 +1,15 @@
+#require 'pry'
 class BreedHighlights::CLI 
 
   def call 
     puts "Welcome! Let's find out more about the most popular breeds in UK."
     #Please select the one you want to see breed highlaghts."
+    #binding.pry
+  end
+  
+  def get_dog_breeds
+    #to be scraped
+    @breeds = ["French Buldog", "English Buldog", "Chichuaua"]
     binding.pry
   end
   
